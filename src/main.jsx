c1e387ec68a42ee43253fc170760f095c99630d1
mayabargig <mayabargig@gmail.com>
2024-04-13 21:33:56 +0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import UserProvider from './src/context/User.jsx'
+import UserProvider from './context/User.jsx'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
