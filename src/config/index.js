05a69a530266c90ca6fb5bc031211b3cb41e1536
mayabargig <mayabargig@gmail.com>
2024-04-13 21:44:09 +0300

    deploy

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
-const APIBaseUrl = "http://localhost:5000/api/v1";
+// const APIBaseUrl = "http://localhost:5000/api/v1";
+const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/?retryWrites=true&w=majority&appName=project1";
 
 export { APIBaseUrl };
