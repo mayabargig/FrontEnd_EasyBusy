0b4c2c3f0c25b69e70f1b33315f0af30eee97396
mayabargig <mayabargig@gmail.com>
2024-05-23 00:20:06 +0300

    github action

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // const APIBaseUrl = "http://localhost:5000/api/v1";
-const APIBaseUrl = process.env.REACT_APP_SERVER_URL;
+const APIBaseUrl = import.meta.env.REACT_APP_SERVER_URL;
 // const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/?retryWrites=true&w=majority&appName=project1";
 
 // const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/";
