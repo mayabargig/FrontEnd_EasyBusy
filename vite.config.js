6c8a6139d83f1685c5bcf9b02e0918841e5fb19c
mayabargig <mayabargig@gmail.com>
2024-05-22 23:55:47 +0300

    github action

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   // refer to git repo path
-  base: '/FrontEnd_EasyBusy/',
+  base: '/FrontEnd_EasyBusy/'.toLocaleLowerCase(),
   plugins: [react()
   ],
   css: {
