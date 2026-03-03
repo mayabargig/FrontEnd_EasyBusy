e97db9782533b94d5b3120141330c0fec55ea9b2
mayab-web <mayab@boardit.io>
2026-03-03 18:10:32 +0200

    update env and db connection

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,10 @@
+// const APIBaseUrl = "http://localhost:5000/api/v1"; 
+// for development
 // const APIBaseUrl = "http://localhost:5000/api/v1";
-const APIBaseUrl = "http://localhost:5000/api/v1";
-// const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/?retryWrites=true&w=majority&appName=project1";
 
+const APIBaseUrl = "mongodb+srv://mayabargig:maya@petzone.db8yaod.mongodb.net/?appName=petzone";
+
+// const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/?retryWrites=true&w=majority&appName=project1";
 // const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/";
 
 export { APIBaseUrl };
