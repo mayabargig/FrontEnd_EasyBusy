e6c1d86e20de70c6c1a9ed9532f920c0afe7b0c7
mayab-web <mayab@boardit.io>
2026-03-03 18:50:19 +0200

    Fix merge conflict in config/index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-const APIBaseUrl = import.meta.env.VITE_EASY_BUSY_BACKEND__URL;
+const APIBaseUrl = import.meta.env.VITE_EASY_BUSY_BACKEND_URL;
 // const APIBaseUrl = "http://localhost:5000/api/v1";
 // const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/?retryWrites=true&w=majority&appName=project1";
-=======
 // const APIBaseUrl = "http://localhost:5000/api/v1"; 
 // for development
 // const APIBaseUrl = "http://localhost:5000/api/v1";
->>>>>>> master
 
-const APIBaseUrl = "mongodb+srv://mayabargig:maya@petzone.db8yaod.mongodb.net/?appName=petzone";
+// const APIBaseUrl = "mongodb+srv://mayabargig:maya@petzone.db8yaod.mongodb.net/?appName=petzone";
 
 // const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/?retryWrites=true&w=majority&appName=project1";
 // const APIBaseUrl = "mongodb+srv://mayabargig:PmLl5g9Lu9JsPB3I@project1.9aqr4f0.mongodb.net/";
